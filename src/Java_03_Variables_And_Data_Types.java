7b015126b95f24c528c5df4be46116cb0b35abe8
Arya <97986884+BumpArya@users.noreply.github.com>
2022-03-02 01:51:20 +0530

    I am not adding records anymore

--- src/Java_03_Variables_And_Data_Types.java
+++ src/Java_03_Variables_And_Data_Types.java
@@ -1,4 +1,4 @@
-public class Java_02_Variables_And_Data_Types {
+public class Java_03_Variables_And_Data_Types {
     public static void main(String[] args) {
         int num1 = 1;
         int num2 = 2;
